b4271d8cc8909487547b6f4b58fa3b6794dc2e1e
Eduardo Rojas <frm@MacBook-Pro-de-Eduardo.local>
2014-11-23 18:55:51 -0600

    correccion base de datos

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'vagrant';
-$db['default']['password'] = 'vagrant';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'loginapp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
